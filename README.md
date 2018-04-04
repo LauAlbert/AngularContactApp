4f83fb13e76939de193e900e0163624b6d4c857e
LauAlbert <albertlau123@gmail.com>
2018-04-04 14:32:03 -0700

    add search by first name

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ![Application Image]
 (https://github.com/LauAlbert/AngularContactApp/blob/master/screenshot/Capture.PNG)
+
+![Application Image]
+(https://github.com/LauAlbert/AngularContactApp/blob/master/screenshot/Capture2.PNG)
--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -13,6 +13,7 @@ export class ContactListComponent implements OnInit {
   maxPage: number;
   currentPage = 1;
   pageSize = 5;
+  searchFirstName = '';
 
   constructor(private personService: PersonService) { }
 
@@ -34,15 +35,28 @@ export class ContactListComponent implements OnInit {
     this.updateList();
   }
 
-  updateList(): void {
+  changePageSize(): void {
     this.currentPage = 1;
-    this.currentPageContacts = this.contacts.slice((this.currentPage - 1) * this.pageSize, this.currentPage * this.pageSize);
-    this.maxPage = Math.ceil(this.contacts.length / this.pageSize);
+    this.updateList();
+
+  }
+
+  searchByFirstName(): void {
+    this.currentPage = 1;
+    this.updateList();
+  }
+
+  updateList(): void {
+
+    const filterContacts = this.contacts.filter(contact => contact.firstName.toLowerCase().includes(this.searchFirstName.toLowerCase()));
+
+    this.currentPageContacts = filterContacts.slice((this.currentPage - 1) * this.pageSize, this.currentPage * this.pageSize);
+    this.maxPage = Math.ceil(filterContacts.length / this.pageSize);
   }
   changePage(page: number): void {
     if (page > 0 && page < this.maxPage + 1) {
       this.currentPage = page;
-      this.currentPageContacts = this.contacts.slice((this.currentPage - 1) * this.pageSize, this.currentPage * this.pageSize);
+      this.updateList();
     }
   }
 
